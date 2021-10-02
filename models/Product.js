3b18ee13816b13dc6e019d0b0bf3bac4380eff4f
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-10-02 06:50:07 +0530

    product updated

--- models/Product.js
+++ models/Product.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    image:{
-        type: String,
-        required: true,
-    },
-    productCode:{
-        type: Date,
-        required: true,
-    },
-    price:{
-        type: Number,
-        required: true,
-    },
-    category:{
-        type: String,
-        require: true,
-    },
-    manufactureDate:{
-        type: Date,
-        required: true,
-    }, 
-    expiryDate:{
-        type: Date,
-        required: true,
-    },
-    owner:{
-        type: String,
-        required: true,
-    },
-    status:{
-        type: String,
-        required: true,
-        default: "available"
-    },
-    priceChanged:{
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  productCode: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  manufactureDate: {
+    type: Date,
+    required: true,
+  },
+  expiryDate: {
+    type: Date,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'available',
+  },
+  priceChanged: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const User = require('./../models/User');
 const Product = require('./../models/Product');
@@ -33,83 +33,92 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // get all products
-router.get('/', async (req,res)=>{
-    try{
-        let products = await Product.find({});
-        return res.send({products});
-    }catch(err){
-        console.error(err);
-        return res.status(500).send('Error Occured');
-    }
+router.get('/', async (req, res) => {
+  try {
+    let products = await Product.find({});
+    products = products.filter(
+      (product) => new Date(product.expiryDate).getTime() > new Date().getTime()
+    );
+    return res.send({ products });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error Occured');
+  }
 });
 
-router.post('/',upload.single('image'), async (req,res)=>{
-    let product = req.body;
-    const file = req.file;
-    console.log(file);
-    if (!file) return res.status(400).send('No image found in the request');
+router.post('/', upload.single('image'), async (req, res) => {
+  let product = req.body;
+  const file = req.file;
+  console.log(file);
+  if (!file) return res.status(400).send('No image found in the request');
 
-    const fileName = req.file.filename;
-    const filePath = '/uploads/' + fileName;
-    // date setting
-    let date = new Date();
-    let yesterday = new Date(date.getTime());
-    yesterday.setDate(date.getDate() - 1);
+  const fileName = req.file.filename;
+  const filePath = '/uploads/' + fileName;
+  // date setting
+  let date = new Date();
+  let yesterday = new Date(date.getTime());
+  yesterday.setDate(date.getDate() - 1);
 
-    product.priceChanged = yesterday;
-    product.image = filePath;
-    try{
-        let newProduct = await Product.create(product);
-        return res.send({newProduct});
-    }catch(err){
-        console.error(err);
-        return res.status(500).send('Error Occured');
-    }
+  product.priceChanged = yesterday;
+  product.image = filePath;
+  try {
+    let newProduct = await Product.create(product);
+    return res.send({ newProduct });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error Occured');
+  }
 });
 
-router.put('/:id', async (req,res)=>{
-    let id = req.params.id;
-    let product = req.body;
-    //product.priceChanged = new Date();
-    if(req.body.price){
-        try{
-            let newProduct = await Product.findByIdAndUpdate({_id: id},{price: req.body.price, 
-            priceChanged: new Date()},{
-                new: true
-            });
-            return res.send({newProduct});
-        }catch(err){
-            console.error(err);
-            return res.status(500).send('Error Occured');
+router.put('/:id', async (req, res) => {
+  let id = req.params.id;
+  let product = req.body;
+  //product.priceChanged = new Date();
+  if (req.body.price) {
+    try {
+      let newProduct = await Product.findByIdAndUpdate(
+        { _id: id },
+        { price: req.body.price, priceChanged: new Date() },
+        {
+          new: true,
         }
-    }else{
-        console.error(err);
-        return res.status(500).send('Error Occured');
+      );
+      return res.send({ newProduct });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send('Error Occured');
     }
+  } else {
+    console.error(err);
+    return res.status(500).send('Error Occured');
+  }
 });
 
-router.delete('/:id', async function(req, res){
-    let id = req.params.id;
-    try{
-        await Product.findByIdAndRemove(id);
-        console.log('success');
-        return res.status(200).send();
-    }catch(err){
-        console.error(err);
-        return res.status(500).send('Error Occured');
-    }
+router.delete('/:id', async function (req, res) {
+  let id = req.params.id;
+  try {
+    await Product.findByIdAndRemove(id);
+    console.log('success');
+    return res.status(200).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error Occured');
+  }
 });
 
 // sort by category
-router.get('/category/:name', async function(req, res){
-    let name = req.params.name;
-    try{
-        let products = await Product.find({category: name});
-        return res.status(200).send({products});
-    }catch(err){
-        console.error(err);
-        return res.status(500).send('Error Occured');
-    }
+router.get('/category/:name', async function (req, res) {
+  let name = req.params.name;
+  try {
+    let products = await Product.find({ category: name });
+    products = products.filter(
+      (product) => new Date(product.expiryDate).getTime() > new Date().getTime()
+    );
+    return res.status(200).send({ products });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error Occured');
+  }
 });
 
 module.exports = router;
